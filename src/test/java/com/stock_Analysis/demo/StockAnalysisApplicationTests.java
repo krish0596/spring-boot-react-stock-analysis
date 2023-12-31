8a17587a0d46b5bc103ce3a96b5b4a5e522c4e58
krish0596 <krish.newatia80@gmail.com>
2023-12-31 22:52:56 +0530

    some changes

---
+++ src/test/java/com/stock_Analysis/demo/StockAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_Analysis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
